35cb53ea07564500b9993d390c78e0726ee10988
markel <markel@tin.it>
2021-12-22 19:32:16 +0100

    update

--- src/utils/concat.go
+++ src/utils/concat.go
@@ -14,43 +14,6 @@
 
 package utils
 
-import (
-	"errors"
-	"reflect"
-	"strings"
-)
-
-func Trim(src string) string {
-	return strings.Trim(src, " \r\n\t")
-}
-
-func Reduce(source, initialValue, reducer interface{}) (interface{}, error) {
-	srcV := reflect.ValueOf(source)
-	kind := srcV.Kind()
-	if kind != reflect.Slice && kind != reflect.Array {
-		return nil, errors.New("source value is not an array")
-	}
-	if reducer == nil {
-		return nil, errors.New("reducer function cannot be nil")
-	}
-	rv := reflect.ValueOf(reducer)
-	if rv.Kind() != reflect.Func {
-		return nil, errors.New("reducer argument must be a function")
-	}
-	accumulator := initialValue
-	accV := reflect.ValueOf(accumulator)
-	for i := 0; i < srcV.Len(); i++ {
-		entry := srcV.Index(i)
-		reduceResults := rv.Call([]reflect.Value{
-			accV,
-			entry,
-			reflect.ValueOf(i),
-		})
-		accV = reduceResults[0]
-	}
-	return accV.Interface(), nil
-}
-
 func Concat(src interface{}) string {
 	var seq []byte
 	data, _ := src.([]interface{})
